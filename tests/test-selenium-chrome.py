8a047fd2e41292cf87a2de43890fe997eb14a34d
lberes <laszlo.beres@nng.com>
2022-09-03 18:55:48 +0200

    OS-1 Fix content

--- tests/test-selenium-chrome.py
+++ tests/test-selenium-chrome.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-class Bereslaszlo(unittest.TestCase):
+class SeleniumTest(unittest.TestCase):
 
     def setUp(self):
         options = Options()
